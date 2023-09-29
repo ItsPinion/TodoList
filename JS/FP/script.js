3aa23efd196898994e07d7a052871afd90e38f0c
PinionsNest <135445676+PinionsNest@users.noreply.github.com>
2023-09-29 23:50:25 +0600

    adding TS

--- JS/FP/script.js
+++ JS/FP/script.js
@@ -1,4 +1,34 @@
-import { addTodo, deleteTodo, toggleDone } from "./script";
+export function addTodo(task) {
+  const todoItem = {
+    task: task,
+    done: false,
+  };
+  return todoItem;
+}
+
+export function deleteTodo(todoList, index) {
+  const newList = [...todoList.slice(0, index), ...todoList.slice(index + 1)];
+  
+  return newList;
+}
+
+export function toggleDone(todoList, index) {
+  const todoItem = {
+    task: todoList[index].task,
+    done: !todoList[index].done,
+  };
+
+  const newList = [
+    ...todoList.slice(0, index),
+    todoItem,
+    ...todoList.slice(index + 1),
+  ];
+
+  return newList;
+}
+
+
+// sideEffect.js
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
